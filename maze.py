08302a43b527e218afc04a6f164c862b7b562965
Jerry Dai <jdai315@gmail.com>
2015-01-23 11:39:25 -0500

    random maze generator complete

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
 #this is our random maze generator
-#2D array in Python with tuple coordinate pairs as keys
+#2D array in Python with list tuple coordinate pairs as keys
 #value for each key is:
 # 0 = empty space
 # 1 = wall
@@ -12,16 +12,34 @@ def maze(n):
     for y in range(n):
         maze_keys = maze_keys + [(x,y) for x in range(n)]
 
-    #show maze as a list of tuples (coordinates)
-    def show_maze():
-        for key in maze_keys:
-            print str(key)
-        print "######################"
-
     #initialize all maze tiles as walls
     maze_coordinates = {}
     for key in maze_keys:
         maze_coordinates[key] = 1
 
+    #show maze as a list of tuples (coordinates)
+    def show_maze_coordinates():
+        for key in maze_keys:
+            print str(key)
+        print "######################"
+
     #return the maze as a dictionary
-    return maze_coordinates
+    def get_maze():
+        return maze_coordinates
+
+    #show maze as a string grid
+
+    maze = ''
+    for y in range(n):
+        for x in range(n):
+            maze_value = maze_coordinates[(x,y)]
+            maze = maze + str(maze_value)
+        maze = maze + "\n"
+    print maze
+
+def maze2(n):
+    total_cells = n*n
+    maze = []
+    for x in range(total_cells):
+        maze.append(x)
+    return maze
