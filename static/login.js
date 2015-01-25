27f1dfcd2d40a3200d36de3c1fd8b8a9607e9f0b
unknown <jdai315@gmail.com>
2015-01-25 02:08:35 -0500

    working login and logout of facebook

--- static/login.js
+++ static/login.js
@@ -1,45 +1,17 @@
+//user
+var username = "";
+
 //Get permission to use the API with our appID
 window.fbAsyncInit = function() {
     FB.init({
 	appId      : '332847230172667',
+	cookie     : true,
 	xfbml      : true,
 	version    : 'v2.1'
     });
 };
 
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response); 
-    if (response.status === 'connected') {
-	connected();
-    } else if (response.status === 'not_authorized') {
-	document.getElementById('status').innerHTML = 'Please log ' +
-	    'into this app.';
-    } else {
-	document.getElementById('status').innerHTML = 'Please log ' +
-	    'into Facebook.';
-    }
-}
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-	statusChangeCallback(response);
-    });
-}
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
+//Load the SDK asynchronously
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
@@ -48,12 +20,48 @@ FB.getLoginStatus(function(response) {
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-//Successful login
-function connected() {
-    console.log('Welcome!  Fetching your information.... ');
+//login if the player tries to connect successfully, or logout
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+	player_connect(response);
+    }else {
+	player_disconnect(response);
+    }
+  }
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+//log the player in, add name to the lobby
+function player_connect(response) {
     FB.api('/me', function(response) {
 	console.log('Successful login for: ' + response.name);
-	document.getElementById('status').innerHTML = response.name;
+	var player = document.createElement('div');
+	player.id = response.name;
+	player.className = response.name;
+	player.innerHTML = response.name;	
+	document.getElementById("players").appendChild(player);
+	username = response.name;
     });
-
 }
+
+//log the player out, remove name from the lobby
+
+function player_disconnect(response) {
+    if (!response.session) {
+	player = document.getElementById(username);
+	player.remove()
+	return;
+    }
+    FB.logout(player_disconnect)
+}
+
